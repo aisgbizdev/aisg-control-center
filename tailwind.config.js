a30d6845cf4440f4c717e06461b320cb48b0fc5f
aisgbizdev <aisgbizdev@gmail.com>
2025-10-30 12:14:30 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,8 @@
-content: ["./index.html", "./src/**/*.{js,jsx}"],
-theme: { extend: {} },
-plugins: [],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
