4f3747da0b2c46b12b5efff03fdfd9bbb23fa0a2
aisgbizdev <aisgbizdev@gmail.com>
2025-10-31 22:18:59 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    port: 4173,
+    host: "0.0.0.0",
+    port: process.env.PORT || 4173,
   },
   preview: {
-    host: '0.0.0.0',
-    port: 4173,
-    strictPort: true,
-    allowedHosts: ['aisg-pro-79ru.onrender.com', 'aisg-control-center.onrender.com'],
+    host: "0.0.0.0",
+    port: process.env.PORT || 4173,
+    allowedHosts: ["aisg-pro-79ru.onrender.com", "aisg-control-center.onrender.com"],
   },
-})
+});
