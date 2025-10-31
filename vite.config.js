f93cf609d8501d3c66ea0abbfa44e3bd07bae606
aisgbizdev <aisgbizdev@gmail.com>
2025-10-31 21:45:23 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
+  preview: {
+    allowedHosts: ['aisg-pro-79ru.onrender.com'],
     port: 4173,
+    strictPort: true,
   },
   preview: {
     allowedHosts: ["aisg-control-center.onrender.com"],
